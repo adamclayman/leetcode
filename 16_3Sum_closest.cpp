22aefc42406f7e947572c01b9d318b7ff24c6b00
Adam Clayman <adam@clayman.org>
2019-09-03 00:32:54 -0400

    Optimized solution to 16_3Sum_closest.cpp

--- 16_3Sum_closest.cpp
+++ 16_3Sum_closest.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        int sum = INT_MAX;
-        int closest = nums[0] + nums[1] + nums[2];
         std::sort(nums.begin(), nums.end());
+        int closest = nums[0] + nums[1] + nums[2];
         int nums_size = nums.size();
-        for (int i = 0; i < nums.size() - 2; i++) {
-            while(nums[i] == nums[i-1]) continue; // Continue if starter is a duplicate
+        for (int i = 0; i < nums.size() - 2; ++i) {
+            // while(nums[i] == nums[i-1]) continue; // Continue if starter is a duplicate            
             int left = i + 1;
             int right = nums_size - 1;
+            int sum;
             while (left < right) {
                 sum = nums[i] + nums[left] + nums[right];
                 if (abs(closest - target) > abs(sum - target)) {
@@ -16,12 +16,12 @@ public:
                 }
                 if (sum < target) {
                     left++;
-                    while (nums[left] == nums[left-1]) left++;
+                    // while (nums[left] == nums[left-1]) left++;
                 } else if (sum > target) {
                     right--;
-                    while (nums[right] == nums[right+1]) right--;
+                    // while (nums[right] == nums[right+1]) right--;
                 } else {
-                    return closest;
+                    return target;
                 }                
             }
         }
